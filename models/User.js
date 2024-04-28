ac0a8ec1e3602bc89fda8fd69a604a18491a0640
Sandra Palacios <sandraanabel21@gmail.com>
2024-04-27 19:23:47 -0500

    Esquema del usuario y modelo

--- models/User.js
+++ models/User.js
@@ -1,3 +1,34 @@
 //Importamos Mongoose para definir y tener el esquema de usuario y el modelo
 
 const mongoose = require("mongoose")
+const { type } = require("os")
+
+//Definimos el esquema de usuario utilizando el constructor de Mongoose llamado Schema.
+
+const userSchema = new mongoose.Schema({
+    nombre : {
+        type: String,
+        required: true  // El nombre es obligatorio
+    },
+    edad : {
+        type: Number,
+        required: true
+    },
+    email : {
+        type : String,
+        required: true,
+        unique: true  //El correo electronico debe de ser unico
+    },
+    contraseña : {
+        type: String,
+        required: true
+
+    } 
+})
+
+//Crear el modelo user utilizando el esquema definido anteriormente
+
+const User= mongoose.model("User", userSchema)
+
+//Exportamos el modelo User para usarlo en cualquier parte
+module.exports= User
