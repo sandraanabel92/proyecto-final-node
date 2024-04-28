2d82032b5d3d1b0a787f184a3506b3d00ecd1424
Sandra Palacios <sandraanabel21@gmail.com>
2024-04-27 20:30:41 -0500

    Creacion de un nuevo usuario

--- controllers/userController.js
+++ controllers/userController.js
@@ -2,13 +2,28 @@
 const User = required("../models/User")
 
 //Funcion para obtner los usuarios
-function getAllUsers(req,res){
+function getAllUsers(req, res) {
     //metodo fin() de Mongoose para encontrar todos los usuarios
 
     User.find()
-    .then(users => res.json(users)) //Se envia todos los usarios como respuesta
-    .catch(err => {
-        console.error(err)
-        res.status(500).send("Error al obtener usuarios")
-    })
+        .then(users => res.json(users)) //Se envia todos los usarios como respuesta
+        .catch(err => {
+            console.error(err)
+            res.status(500).send("Error al obtener usuarios")
+        })
+}
+
+//Funcion para creacion de un usuario
+function createUser(req, res) {
+    //Extraer la informacion del cuerpo de la solicitud
+
+    const { nombre, edad, email, contraseña } = req.body
+
+    //Creacion de un nuevo usuario con el metodo create()
+    User.create({ nombre, edad, email, contraseña })
+        .then(newUser => res.json(newUser))
+        .catch(err => {
+            console.error(err)
+            res.status(500).send("Error al crear un nuevo Usuario")
+        })
 }
