c507a22df948fa54bb647e1bb0f76351ca8d26cb
Sandra Palacios <sandraanabel21@gmail.com>
2024-04-28 21:56:42 -0500

    Creacion del verifyToken

---
+++ middleware/verifyToken.js
@@ -0,0 +1,23 @@
+const jwt = require("jsonwebtoken")
+
+function verifyToken(req, res, next) {
+    return new Promise((resolve, reject) => {
+        const token = req.headers.authorization
+
+        if (!token) {
+            reject({ status: 401, message: "Token de autenticacion no proporcionado" })
+        }
+        jwt.verify(token.split(" ")[1], "f011d2ebb9a42007640f7c108dcfa702a25a0121e493cbfe423a11995f9abe4e",
+            (error, decodedToken) => {
+                if (error) {
+                    reject({ status: 401, message: "Token de autenticacion no valido" })
+                } else {
+                    req.userId = decodedToken.userId  //Agregamos el Id de usuario decodificado para su posterior uso
+                    resolve()
+                }
+            })
+    })
+        .then(() => next()) //Continua el seguimiento del siguiente middleware o del siguiente controlador
+        .catch(error => res.status(error.status || 500).json({ message: error.message }))
+}
+module.exports = verifyToken
