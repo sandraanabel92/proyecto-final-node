fd19d07b0cae29f977f25bcb26d11b139bc94a15
Sandra Palacios <sandraanabel21@gmail.com>
2024-05-03 22:12:17 -0500

    Creación del bcryptService

---
+++ services/bcryptService.js
@@ -0,0 +1,39 @@
+const bcrypt = require("bcrypt");
+
+//Funcion para hashear una contraseña
+
+function hashPassword(plainPassword) {
+    return new Promise((resolve, reect) => {
+        const saltRounds = 10;
+        bcrypt.hash(plainPassword, saltRounds, (error, hashedPassword) => {
+            if (error) {
+                PromiseRejectionEvent(new Error("Error al hashear la contraseña"))
+            } else {
+                resolve(hashedPassword)
+            }
+
+        })
+    })
+}
+
+
+//Funcion para comparar una contraseña con su contraseña encriptada
+
+function comparePassword(plainPassword, hashedPassword) {
+    return new Promise((resolve, reject) => {
+        bcrypt.compare(plainPassword, hashedPassword, (error, match) => {
+            if (error) {
+                reject(new Error("Error al comparar contraseñas"))
+            }
+            else {
+                resolve(match)
+            }
+
+        })
+    })
+}
+
+module.exports = {
+    hashPassword,
+    comparePassword
+}
